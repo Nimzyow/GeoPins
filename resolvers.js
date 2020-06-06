9fd1ac94f9c59296df92d0132985755275c7d0d5
Nimzyow <n_soufiani@hotmail.com>
2020-06-06 21:20:27 +0100

    refactored

--- resolvers.js
+++ resolvers.js
@@ -3,11 +3,11 @@ const user = {
   _id: "1",
   name: "Nima",
   email: "n_soufiani@hotmail.com",
-  picture: "https://cloudinary.com/adijo"
+  picture: "https://cloudinary.com/adijo",
 };
 
 module.exports = {
   Query: {
-    me: () => user
-  }
+    me: () => user,
+  },
 };
--- server.js
+++ server.js
@@ -6,18 +6,29 @@ const resolvers = require("./resolvers");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
-  .then(() => {
+const connectToDb = async () => {
+  try {
+    await mongoose.connect(process.env.GEOPINS_TEST_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    });
     console.log("DB connected");
-  })
-  .catch(err => console.error(err));
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
 
+connectToDb();
+
 server.listen().then(({ url }) => {
-  console.log(`server listening on ${url}`);
+  console.log(
+    `server listening on ${url}. 
+Environment is ${process.env.NODE_ENV}`,
+  );
 });
